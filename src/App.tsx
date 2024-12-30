df749daf848846a226cb51176ae7086976a976af
VitalinaKorchova <vitalina.korchova@gmail.com>
2024-12-30 21:07:18 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <div className="containerApp">
+        <div className="blockLogo">
+          <span className="nameGame">Морський бій</span>
+          <button type="button" className="buttonStart">
+            Почати гру
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
