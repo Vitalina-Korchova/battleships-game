bb6374f7143f616f4ebac007904eb8431f30b315
VitalinaKorchova <vitalina.korchova@gmail.com>
2025-01-09 17:22:18 +0200

    update ship component

--- src/components/Battlefield.tsx
+++ src/components/Battlefield.tsx
@@ -1,18 +1,10 @@
 import styles from "./BattlefieldItems.module.css";
-import { useDroppable } from "@dnd-kit/core";
 
-export default function Battlefield({ id, children }) {
+export default function Battlefield() {
   const cells = Array.from({ length: 100 }, (_, i) => i);
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
-  const { isOver, setNodeRef } = useDroppable({
-    id: id,
-  });
-
-  const style = {
-    backgroundColor: isOver ? "lightgreen" : "#c9d9f1",
-  };
   return (
     <>
       <div className={styles.wrapperBattlefield}>
@@ -36,14 +28,7 @@ export default function Battlefield({ id, children }) {
           </div>
           <div className={styles.battlefield}>
             {cells.map((cell) => (
-              <div
-                ref={setNodeRef}
-                style={style}
-                key={cell}
-                className={styles.cell}
-              >
-                {children}
-              </div>
+              <div key={cell} className={styles.cell}></div>
             ))}
           </div>
         </div>
--- src/components/PlaceShipsBlock.tsx
+++ src/components/PlaceShipsBlock.tsx
@@ -2,32 +2,18 @@ import styles from "./BattlefieldItems.module.css";
 import Battlefield from "./Battlefield";
 import Ship from "./Ship";
 import { useState } from "react";
-import { DndContext } from "@dnd-kit/core";
+
 export default function PlaceShipsBlock() {
-  const [shipPosition, setShipPosition] = useState({});
-
-  const handleDragEnd = (event) => {
-    const { active, over } = event;
-    if (over) {
-      setShipPosition((prev) => ({
-        ...prev,
-        [active.id]: over.id,
-      }));
-    }
-  };
-
   return (
     <>
       <div className={styles.container}>
-        <DndContext onDragEnd={handleDragEnd}>
-          <Battlefield />
-          <div>
-            <Ship id={"ship1"} amount={1} />
-            <Ship id={"ship2"} amount={2} />
-            <Ship id={"ship3"} amount={3} />
-            <Ship id={"ship4"} amount={4} />
-          </div>
-        </DndContext>
+        <Battlefield />
+        <div>
+          <Ship amount={1} />
+          <Ship amount={2} />
+          <Ship amount={3} />
+          <Ship amount={4} />
+        </div>
       </div>
     </>
   );
