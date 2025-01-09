02241b44d4c206277da52bec04b2030bd8675534
VitalinaKorchova <vitalina.korchova@gmail.com>
2025-01-09 17:16:57 +0200

    update ship component

--- src/components/Ship.tsx
+++ src/components/Ship.tsx
@@ -3,35 +3,57 @@ import styles from "./Ship.module.css";
 export default function Ship({ amount }: { amount: number }) {
   if (amount === 1) {
     return (
-      <div className={styles.singleShip}>
-        <div className={styles.circle}></div>
+      <div className={styles.innerShip}>
+        <div className={styles.singleShip}>
+          <div className={styles.circle}></div>
+        </div>
+      </div>
+    );
+  }
+  if (amount === 2) {
+    return (
+      <div className={styles.innerShip}>
+        <div className={styles.headShip}>
+          <div className={styles.circle}></div>
+        </div>
+        <div className={styles.tailShip}>
+          <div className={styles.circle}></div>
+        </div>
+      </div>
+    );
+  }
+  if (amount === 3) {
+    return (
+      <div className={styles.innerShip}>
+        <div className={styles.headShip}>
+          <div className={styles.circle}></div>
+        </div>
+        <div className={styles.bodyShip}>
+          <div className={styles.circle}></div>
+        </div>
+        <div className={styles.tailShip}>
+          <div className={styles.circle}></div>
+        </div>
       </div>
     );
   }
 
-  return (
-    <div>
-      <div className={styles.headShip}>
-        <div className={styles.circle}></div>
-      </div>
-
-      {amount >= 3 && (
+  if (amount === 4) {
+    return (
+      <div className={styles.innerShip}>
+        <div className={styles.headShip}>
+          <div className={styles.circle}></div>
+        </div>
         <div className={styles.bodyShip}>
           <div className={styles.circle}></div>
         </div>
-      )}
-
-      {amount === 4 && (
         <div className={styles.bodyShip}>
           <div className={styles.circle}></div>
         </div>
-      )}
-
-      {amount >= 2 && (
         <div className={styles.tailShip}>
           <div className={styles.circle}></div>
         </div>
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
 }
